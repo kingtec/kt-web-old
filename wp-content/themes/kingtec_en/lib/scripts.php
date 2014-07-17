1b288337712b342fc0fb271ee0baa75621b52300
Carlo Holl <carloholl@gmail.com>
2014-07-17 17:35:49 +0800

    css edits [deploy: kingtec_server]

--- wp-content/themes/kingtec_en/lib/scripts.php
+++ wp-content/themes/kingtec_en/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '17b04b5a6aa6aaadcf939b40a559f407');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '463385341ecd41290b5c2491405c8adb');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
